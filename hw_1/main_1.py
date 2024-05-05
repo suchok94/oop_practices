6303a8ec2c388abd62409dabb84fdb60a4412232
suchok94 <suchok94@gmail.com>
2024-05-05 12:24:59 +0300

    made methods of class "Book"

--- hw_1/main_1.py
+++ hw_1/main_1.py
@@ -1,10 +1,15 @@
 # task № 2
 class Book:
-    def __init__(self):
-        pass
+    def __init__(self, name: str, author: str, pages: int):
+        self.name = name
+        self.author = author
+        self.pages = pages
 
-    def open_book(self):
-        pass
+    def open_book(self, pages):
+        if self.pages < pages:
+            print('Книга не может быть открыта на этой странице!')
+        else:
+            print(f'Книга открыта на странице номер {pages}!')
 
     def info_about_book(self):
-        pass
+        print(f'Информация о книге:\n Наименование: {self.name}\nАвтор: {self.author}\nКоличество страниц: {self.pages}')
