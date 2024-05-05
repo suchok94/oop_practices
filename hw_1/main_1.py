fa7cf9e969505dae2d23c3e1ea07f9d55edf344e
suchok94 <suchok94@gmail.com>
2024-05-05 11:57:17 +0300

    made methods of class "Animal"

--- hw_1/main_1.py
+++ hw_1/main_1.py
@@ -1,12 +1,18 @@
 # task № 1
 class Animal:
-    def __init__(self):
-        pass
+    def __init__(self, name: str, type: str, age: int, sound: str):
+        self.name = name
+        self.type = type
+        self.age = age
+        self.sound = sound
+
 
     def sound_animal(self):
-        pass
+        print(f'Животное с именем {self.name} издаёт звук: "{self.sound}"')
 
     def info_of_animal(self):
-        pass
+        print(f'Имя животного: {self.name}')
+        print(f'Вид животного: {self.type}')
+        print(f'Его возраст: {self.age}')
+        
 
-    
