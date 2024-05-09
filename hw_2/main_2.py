e27509ea6bcb740ee68450c635e58eeef12f9c8d
suchok94 <suchok94@gmail.com>
2024-05-09 12:44:10 +0300

    make methods for class "Patient"

--- hw_2/main_2.py
+++ hw_2/main_2.py
@@ -3,12 +3,20 @@ import datetime
 class Patient:
 
     def __init__(self, surname: str, name: str, patronymic: str, age: int, disease: str):
-        pass
+        self.surname = surname
+        self.name = name
+        self.patronymic = patronymic
+        self.age = age
+        self.disease = disease
 
     def make_an_appointment(self, date: date, time: date):
-        pass
+        print(f'Пациент {self.surname} {self.name} {self.patronymic} записан на приём {date} в {time}')
 
     def __str__(self):
-        pass
+        return f'Информация о пациенте:' \
+               f'ФИО: {self.surname, self.name, self.patronymic}' \
+               f'Возраст: {self.age}' \
+               f'Текущий диагноз: {self.disease}'
+    
 
 
