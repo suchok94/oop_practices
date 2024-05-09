fe3a7ca7c54e203b57380439183c03d7a16a8d21
suchok94 <suchok94@gmail.com>
2024-05-09 13:02:50 +0300

    add annotirovanie

--- hw_2/main_2.py
+++ hw_2/main_2.py
@@ -5,6 +5,14 @@ import datetime
 class Patient:
 
     def __init__(self, surname: str, name: str, patronymic: str, age: int, disease: str):
+        '''
+        Создаёт пациента
+        :param surname:
+        :param name:
+        :param patronymic:
+        :param age:
+        :param disease:
+        '''
         self.surname = surname
         self.name = name
         self.patronymic = patronymic
@@ -12,9 +20,19 @@ class Patient:
         self.disease = disease
 
     def make_an_appointment(self, date: date, time: time):
+        """
+        принимает дату и время приёма, выводит информацию о приёме
+        :param date:
+        :param time:
+        :return:
+        """
         print(f'Пациент {self.surname} {self.name} {self.patronymic} записан на приём {date} в {time}')
 
     def __str__(self):
+        """
+        Описывает информацию о пациенте
+        :return: str, инфомарция о пациенте
+        """
         return f'Информация о пациенте:\n' \
                f'ФИО: {self.surname} {self.name} {self.patronymic}\n' \
                f'Возраст: {self.age}\n' \
@@ -24,10 +42,14 @@ class Patient:
 class Program:
     @staticmethod
     def main():
+        """
+        Запускает программу
+        :return:
+        """
         p1 = Patient('Petrov', 'Ivan', 'Ivanov', 20, 'computernaya zavisimost')
         data = datetime.date(2024, 5, 11)
         time = datetime.time(2, 20)
         p1.make_an_appointment(data, time)
         print(p1.__str__())
 
-    Program.main()
+Program.main()
