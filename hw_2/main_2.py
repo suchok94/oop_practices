78e4a0458396469329029545c2be8cdc6e4f3070
suchok94 <suchok94@gmail.com>
2024-05-09 12:41:38 +0300

    create methods for class Patient

---
+++ hw_2/main_2.py
@@ -0,0 +1,14 @@
+import datetime
+# task 1
+class Patient:
+
+    def __init__(self, surname: str, name: str, patronymic: str, age: int, disease: str):
+        pass
+
+    def make_an_appointment(self, date: date, time: date):
+        pass
+
+    def __str__(self):
+        pass
+
+
