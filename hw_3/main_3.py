7cbfe8faf54249ed363fcabf89bb20ac6de2a3e8
suchok94 <suchok94@gmail.com>
2024-05-23 10:01:14 +0300

    add getters methods for class Wizzard

--- hw_3/main_3.py
+++ hw_3/main_3.py
@@ -3,10 +3,26 @@ class Wizard:
 
     def __init__(self, name: str, faculty: str, lvl_power: int, list_spells: list[Spell], graduate: bool):
         self.__name = name
-        self.__facultu = faculty
+        self.__faculty = faculty
         self.__lvl_power = lvl_power
         self.__list_spells = list_spells
         self.__graduate = graduate
+
+    def get_name(self):
+        return self.__name
+
+    def get_house(self):
+        return self.__faculty
+
+    def get_magic_level(self):
+        return self.__lvl_power
+
+    def get_spells(self):
+        return self.__list_spells
+
+    def get_status(self):
+        return self.__graduate
+    
 
 
 
