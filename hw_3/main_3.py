7239c557a73ff95accb859873125a13d430a6076
suchok94 <suchok94@gmail.com>
2024-05-23 10:47:58 +0300

    add other methods for class Wizzard

--- hw_3/main_3.py
+++ hw_3/main_3.py
@@ -40,11 +40,37 @@ class Wizard:
         if status.lower() == 'выпущен':
             self.__graduate = True
         elif status.lower() == 'в хогвартсе':
-            self.__graduate == False
+            self.__graduate = False
         else:
             raise Exception('Error message')
 
-    
+    def add_spell(self, spell: Spell):
+        if not(spell in self.__list_spells):
+            self.__list_spells.append(spell)
+
+
+    def remove_spell(self, spell: Spell):
+        if spell in self.__list_spells:
+            self.__list_spells.remove(spell)
+
+
+    def increase_magic_level(self, amount: int):
+        if not isinstance(amount, int):
+            raise Exception("amount isn't int")
+
+        if amount > 0:
+            self.__magic_level += amount
+        else:
+            raise Exception("amount <=0")
+
+    def __str__(self):
+        return f'Имя волшебника: {self.__name}' \
+               f'Факултет: {self.__house}' \
+               f'Уровень магической силы: {self.__magic_level}' \
+               f'Список известных заклинаний: {self.__list_spells}' \
+               f'Статус выпуска: {self.__graduate}'
+
+
 
 
 
