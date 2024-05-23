3066e19888c7e5b3e073b0bec43f0017aff5e585
suchok94 <suchok94@gmail.com>
2024-05-23 10:28:26 +0300

    add settors methods for class Wizzard

--- hw_3/main_3.py
+++ hw_3/main_3.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 class Wizard:
 
-    def __init__(self, name: str, faculty: str, lvl_power: int, list_spells: list[Spell], graduate: bool):
+    def __init__(self, name: str, house: str, magic_level: int, list_spells: list[Spell], graduate: bool):
         self.__name = name
-        self.__faculty = faculty
-        self.__lvl_power = lvl_power
+        self.__house = house
+        self.__magic_level = magic_level
         self.__list_spells = list_spells
         self.__graduate = graduate
 
@@ -12,17 +12,40 @@ class Wizard:
         return self.__name
 
     def get_house(self):
-        return self.__faculty
+        return self.__house
 
     def get_magic_level(self):
-        return self.__lvl_power
+        return self.__magic_level
 
     def get_spells(self):
         return self.__list_spells
 
     def get_status(self):
         return self.__graduate
+
+    def set_house(self, house: str):
+        self.__house = house
+
+
+    def set_magic_level(self, magic_level: int):
+        if not isinstance(magic_level, int):
+            raise Exception("magic level isn't int")
+
+        if magic_level > 0:
+            self.__magic_level = magic_level
+        else:
+            raise Exception("magic level <= 0")
+
+    def set_status(self, status: str):
+        if status.lower() == 'выпущен':
+            self.__graduate = True
+        elif status.lower() == 'в хогвартсе':
+            self.__graduate == False
+        else:
+            raise Exception('Error message')
+
     
+
 
 
 
