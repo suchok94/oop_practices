e64cd1aa3ebc0c7115692ab0af0f7abbdce5bd15
suchok94 <suchok94@gmail.com>
2024-05-31 10:18:39 +0300

    task 2, create file main_4_task2.py, add methods in class Library

---
+++ hw_4/task 2/main_4_task2.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+# task 2
+class Library:
+
+    def __init__(self, name: str, address: str, books_list: list[Book], employees_list: list[Employee]):
+        pass
+
+    def get_name(self):
+        pass
+
+    def get_address(self):
+        pass
+
+    def get_books(self):
+        pass
+
+    def get_employees(self):
+        pass
+
+    def set_adress(self, address):
+        pass
+
+    def add_book(self, book):
+        pass
+
+    def remove_book(self, book):
+        pass
+
+    def add_employee(self, employee):
+        pass
+
+    def remove_employee(self, employee):
+        pass
+
+    def __str__(self):
+        pass
