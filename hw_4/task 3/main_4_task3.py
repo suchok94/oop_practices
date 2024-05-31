ac5c64e36014e00eca4d23c2d169e4b013cde0dc
suchok94 <suchok94@gmail.com>
2024-05-31 20:01:12 +0300

    create file task3.drawio and main_4_task3.py

---
+++ hw_4/task 3/main_4_task3.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+# task 3
+
+class Dealership:
+    pass
+
+
+class Salesperson:
+    pass
+
+class Car:
+    pass
+
+class Customer:
+    pass
+
+class Program:
+
+    @staticmethod
+    def main():
+        pass
+
