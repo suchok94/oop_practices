70b8bf586dd554e2a5d767d6ee961f9e58bcf272
suchok94 <suchok94@gmail.com>
2024-05-31 22:38:43 +0300

    add methods in class Dealership

--- hw_4/task 3/main_4_task3.py
+++ hw_4/task 3/main_4_task3.py
@@ -2,7 +2,35 @@ from __future__ import annotations
 # task 3
 
 class Dealership:
-    pass
+
+    def __init__(self, list_cars: list[Car], list_salesperson: list[Salesperson], list_customers: list[Customer])
+        self.__list_cars = list_cars
+        self.__list_salesperson = list_salesperson
+        self.__list_customers = list_customers
+
+    def add_car(self):
+        pass
+
+    def add_salesperson(self):
+        pass
+
+    def add_customer(self):
+        pass
+
+    def remove_car(self):
+        pass
+
+    def remove_salesperson(self):
+        pass
+
+    def get_list_cars(self):
+        pass
+
+    def get_list_customers(self):
+        pass
+
+    def get_list_salesperson(self):
+        pass
 
 
 class Salesperson:
