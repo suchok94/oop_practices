25daa35369765069a69ce696e49d4053c5b6ebc7
suchok94 <suchok94@gmail.com>
2024-05-31 10:40:11 +0300

    make methods in class Library

--- hw_4/task 2/main_4_task2.py
+++ hw_4/task 2/main_4_task2.py
@@ -2,35 +2,73 @@ from __future__ import annotations
 # task 2
 class Library:
 
-    def __init__(self, name: str, address: str, books_list: list[Book], employees_list: list[Employee]):
-        pass
+    def __init__(self, name: str, address: str, books_list: list[Book] = None, employees_list: list[Employee] = None):
+        self.__name = name
+        self.__address = address
+
+        if not (books_list is None):
+            self.__books_list = books_list
+        else:
+            self.__books_list = []
+
+        if not (employees_list is None):
+            self.__employees_list = employees_list
+        else:
+            self.__employees_list = []
 
     def get_name(self):
-        pass
+        return self.__name
 
     def get_address(self):
-        pass
+        return self.__address
 
     def get_books(self):
-        pass
+        return self.__books_list
 
     def get_employees(self):
-        pass
+        return self.__employees_list
 
-    def set_adress(self, address):
-        pass
+    def set_address(self, address):
+        self.__address = address
 
     def add_book(self, book):
-        pass
+        if not isinstance(book, Book):
+            raise Exception('Error type')
+
+        if not (book in self.__books_list):
+            self.__books_list.append(book)
+
 
     def remove_book(self, book):
-        pass
+        if not isinstance(book, Book):
+            raise Exception('Error type')
+
+        if book in self.__books_list:
+            self.__books_list.remove(book)
 
     def add_employee(self, employee):
-        pass
+        if not isinstance(employee, Employee):
+            raise Exception('Error type')
+
+        if not (employee in self.__employees_list):
+            self.__employees_list.append(employee)
 
     def remove_employee(self, employee):
-        pass
+        if not isinstance(employee, Employee):
+            raise Exception('Error type')
+
+        if employee in self.__employees_list:
+            self.__employees_list.remove(employee)
 
     def __str__(self):
-        pass
+        return f'Имя: {self.__name}\n' \
+               f'Адрес: {self.__address}\n' \
+               f'Список книг: {self.__books_list}\n' \
+               f'Список работников: {self.__employees_list}\n'
+       
+
+class Book:
+    pass
+
+class Employee:
+    pass
