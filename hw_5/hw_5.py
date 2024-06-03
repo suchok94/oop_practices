6455c480f9159b952c61bae124c32afa51738cfd
suchok94 <suchok94@gmail.com>
2024-06-03 13:31:56 +0300

    create class Dog, Cat, Bird, Program and make methods in this classes

--- hw_5/hw_5.py
+++ hw_5/hw_5.py
@@ -13,3 +13,35 @@ class Animal:
         return f'Animal make sound'
 
 
+class Dog(Animal):
+
+    def __init__(self, name: str, species: str):
+        Animal.__init__(self, name, species)
+
+    def make_sound(self):
+        return f'Dog make Woof'
+
+
+class Cat(Animal):
+    def make_sound(self):
+        return f'Cat make Meow'
+
+
+class Bird(Animal):
+    def make_sound(self):
+        return f'Bird make ChirikChirik'
+
+class Program:
+
+    @staticmethod
+    def main():
+        dog1 = Dog('Bobik', 'Recks')
+        cat1 = Cat('Murka', 'Siamskaya')
+        bird1 = Bird('Kesha', 'Popugai')
+
+        print(dog1.make_sound())
+        print(cat1.make_sound())
+        print(bird1.make_sound())
+
+
+Program.main()
