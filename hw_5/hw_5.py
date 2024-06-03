fc3cd36effb9b3908711e2878edccd307166f570
suchok94 <suchok94@gmail.com>
2024-06-03 18:59:25 +0300

    task2 create class Person

--- hw_5/hw_5.py
+++ hw_5/hw_5.py
@@ -1,47 +1,66 @@
-# task 1
+# # task 1
+#
+# class Animal:
+#
+#     _name: str
+#     _species: str
+#
+#     def __init__(self, name: str, species: str):
+#         self._name = name
+#         self._species = species
+#
+#     def make_sound(self):
+#         return f'Animal make sound'
+#
+#
+# class Dog(Animal):
+#
+#     def __init__(self, name: str, species: str):
+#         Animal.__init__(self, name, species)
+#
+#     def make_sound(self):
+#         return f'Dog make Woof'
+#
+#
+# class Cat(Animal):
+#     def make_sound(self):
+#         return f'Cat make Meow'
+#
+#
+# class Bird(Animal):
+#     def make_sound(self):
+#         return f'Bird make ChirikChirik'
 
-class Animal:
+
+
+
+# task 2
+class Person:
 
     _name: str
-    _species: str
+    _age: int
 
-    def __init__(self, name: str, species: str):
+    def __init__(self, name: str, age: int):
         self._name = name
-        self._species = species
+        self._age = age
 
-    def make_sound(self):
-        return f'Animal make sound'
+    def introduce_yourself(self):
+        pass
 
-
-class Dog(Animal):
-
-    def __init__(self, name: str, species: str):
-        Animal.__init__(self, name, species)
-
-    def make_sound(self):
-        return f'Dog make Woof'
-
-
-class Cat(Animal):
-    def make_sound(self):
-        return f'Cat make Meow'
-
-
-class Bird(Animal):
-    def make_sound(self):
-        return f'Bird make ChirikChirik'
+    
 
 class Program:
 
     @staticmethod
     def main():
-        dog1 = Dog('Bobik', 'Recks')
-        cat1 = Cat('Murka', 'Siamskaya')
-        bird1 = Bird('Kesha', 'Popugai')
-
-        print(dog1.make_sound())
-        print(cat1.make_sound())
-        print(bird1.make_sound())
+        # # task1
+        # dog1 = Dog('Bobik', 'Recks')
+        # cat1 = Cat('Murka', 'Siamskaya')
+        # bird1 = Bird('Kesha', 'Popugai')
+        #
+        # print(dog1.make_sound())
+        # print(cat1.make_sound())
+        # print(bird1.make_sound())
 
 
 Program.main()
